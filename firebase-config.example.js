ce8a9c997f1e100e54e55db1c5561ed7c4df3272
Rydnichanin <nikonserega196@gmail.com>
2026-09-13 05:00:36 +0500

    Добавить шаблон конфигурации Firebase

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+// Скопируйте этот файл в firebase-config.js и вставьте конфигурацию своего Firebase-проекта.
+// Реальные ключи Firebase Web обычно можно хранить во фронтенде; доступ защищается Firestore Rules.
+export const firebaseConfig = {
+  apiKey: "ВАШ_API_KEY",
+  authDomain: "ВАШ_PROJECT.firebaseapp.com",
+  projectId: "ВАШ_PROJECT_ID",
+  storageBucket: "ВАШ_PROJECT.firebasestorage.app",
+  messagingSenderId: "ВАШ_SENDER_ID",
+  appId: "ВАШ_APP_ID"
+};
