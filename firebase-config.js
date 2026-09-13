6b9ae2ea62e2da879c2ae1aaaf2565eb178b69b6
Rydnichanin <nikonserega196@gmail.com>
2026-09-13 05:06:30 +0500

    Подключить конфигурацию Firebase проекта todace-df1af

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Конфигурация Firebase проекта todace-df1af
+// Эти данные используются браузером. Безопасность обеспечивается правилами Firestore/Auth.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCL6-EtOKTlXcuM0ayOkJ_s7k-sZ3BdmYg",
+  authDomain: "todace-df1af.firebaseapp.com",
+  projectId: "todace-df1af",
+  storageBucket: "todace-df1af.firebasestorage.app",
+  messagingSenderId: "87380622450",
+  appId: "1:87380622450:web:4f8df013151c78aaa4d6eb"
+};
